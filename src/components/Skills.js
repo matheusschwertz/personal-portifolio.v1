00a350b64660db64ccbe5486b9d89bfd526fb86d
matheusschwertz <matheusschwertz@gmail.com>
2023-10-26 20:55:15 -0300

    feat: adding icons in my skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import meter4 from "../assets/img/meter4.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -50,8 +51,8 @@ export const Skills = () => {
                                 <h5>Terraform</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>AWS</h5>
+                                <img src={meter4} alt="Image" />
+                                <h5>Linux</h5>
                             </div>
                         </Carousel>
                     </div>
